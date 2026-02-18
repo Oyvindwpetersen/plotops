f59fae817943c69a1ce3e23d34eb35e2b97be67f
Øyvind <oyvind.w.petersen@ntnu.no>
2026-02-18 21:52:32 +0100

    allow short

--- plotops/__init__.py
+++ plotops/__init__.py
@@ -10,5 +10,29 @@ Created on Wed Dec  1 11:39:19 2021
 from . import misc
 from . import plot
 from . import print
+from . import figure
 
-__all__ = ['misc','plot','print']
+
+# Plotting
+from .plot import plotxy as multiplot   # or rename in plot.py directly
+from .plot import plot3d
+
+# Layout / figure tools
+from .figure import layout, subplot, axistight, size, tile
+
+# Saving / printing
+from .print import savefig
+
+# Optional: small utilities you commonly use
+from .misc import color
+
+__all__ = [
+    'multiplot',
+    'layout',
+    'subplot',
+    'axistight',
+    'size',
+    'tile',
+    'savefig',
+    'color'
+]
