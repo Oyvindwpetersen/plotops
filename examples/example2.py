e8d3f17ab2079dc9a66568e73d825bd848f2a8b5
Øyvind <oyvind.w.petersen@ntnu.no>
2026-03-15 20:52:24 +0100

    example

---
+++ examples/example2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 22 21:32:25 2025
+
+@author: oyvinpet
+"""
+
+#%%
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append(r'C:\Cloud\OD_OWP\Work\Python\Github')
+
+import plotops
+
+#%%
+
+plt.close('all')
+''
+n1, n2 = 40, 50
+x1 = np.linspace(0.1, 10.0, n1)
+x2 = np.linspace(0.2, 6.0, n2)
+X1, X2 = np.meshgrid(x1, x2, indexing='ij')
+F = np.sin(X1) * np.cos(0.7 * X2) + 0.2 * X1
+
+x = np.column_stack([X1.ravel(), X2.ravel()])
+f = F.ravel()
+
+plt.figure()
+
+plotops.plot.surfiso(x, f, xlabel='x1',ylabel='x2',zlabel='f',isolines=20,cmap='coolwarm',markercolor=[0,0,0],facealpha=0.85,view=(40, 15))
+
+# cbar=True,
+# markersize=None)
